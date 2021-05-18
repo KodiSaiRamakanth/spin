13d82cb2ebfab34c56b3088444b771719bb55282
kodiramakanth <kodiramakanth@gmail.com>
2021-05-18 18:46:34 +0530

    first commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,141 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import Swal from 'sweetalert2';
+import { NgxWheelComponent } from 'ngx-wheel';
+import { OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
+
+  @ViewChild(NgxWheelComponent, { static: false }) wheel;
+
   title = 'spin';
-  idToLandOn = 3;
+  idToLandOn=1;
+  list:any=[];
+  str:any;
+  str1 =new String("You have won: ");
   items = [
     {id:1, text:'Prize 1'},
     {id:2, text:'Prize 2'},
     {id:3, text:'Prize 3'},
     {id:4, text:'Prize 4'},
-    {id:5, text:'Prize 5'},
-    {id:6, text:'Prize 6'},
-    {id:7, text:'Prize 7'},
-    {id:8, text:'Prize 8'},
+   
   ];
+  readonly baseURL = 'https://localhost:44358/api/EmployeeDetails'
+
+
+  constructor(private http:HttpClient) {}
+  ngOnInit(): void {
+
+    this.http.get("https://prize.azurewebsites.net/api/EmployeeDetails").subscribe(res =>{
+      this.list=res;
+    }
+    
+   );
+  
+
+}
   before(){
     console.log('before');
+    console.log(this.list);
+
   }
   after(){
-    console.log('after');
-    alert(`You got Prize ${this.idToLandOn}`);
+    console.log(this.idToLandOn);
+    this.str=this.idToLandOn;
+    var str3 = this.str1.concat(this.list[0].employeeName.toString());
+
+
+    if(this.idToLandOn == 1){
+
+      Swal.fire({
+        title: 'Congrats',
+        text: str3,
+        icon: 'success',
+        showCancelButton: false,
+        confirmButtonText: 'Play Again',
+      }).then((result) => {
+  
+        if (result.isConfirmed) {
+  
+          console.log('Clicked Yes, File deleted!');
+          this.myreset();
+          this.wheel.reset();
+  
+  
+        }
+      })
+
+    }else if(this.idToLandOn == 2){
+      var str3 = this.str1.concat(this.list[0].employeeAge.toString());
+      Swal.fire({
+        title: 'Congrats',
+        text: str3,
+        icon: 'success',
+        showCancelButton: false,
+        confirmButtonText: 'Play Again',
+      }).then((result) => {
+  
+        if (result.isConfirmed) {
+  
+          this.myreset();
+          this.wheel.reset();
+  
+  
+        }
+      })
+
+    }else if(this.idToLandOn == 3){
+
+      var str3 = this.str1.concat(this.list[0].employeeDeparment.toString());
+      Swal.fire({
+        title: 'Congrats',
+        text: str3,
+        icon: 'success',
+        showCancelButton: false,
+        confirmButtonText: 'Play Again',
+      }).then((result) => {
+  
+        if (result.isConfirmed) {
+  
+          this.myreset();
+          this.wheel.reset();
+  
+  
+        }
+      })
+
+    }else if(this.idToLandOn == 4){
+      var str3 = this.str1.concat(this.list[0].employeeRole.toString());
+Swal.fire({
+        title: 'Congrats',
+        text: str3,
+        icon: 'success',
+        showCancelButton: false,
+        confirmButtonText: 'Play Again',
+      }).then((result) => {
+  
+        if (result.isConfirmed) {
+  
+          this.myreset();
+          this.wheel.reset();
+  
+  
+        }
+      })
+
+    }
+
+
+    
+  
+    // alert(`You got Prize ${this.idToLandOn}`);
   }
   myreset(){
     console.log('myreset');
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxWheelModule } from 'ngx-wheel';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxWheelModule
+    NgxWheelModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
